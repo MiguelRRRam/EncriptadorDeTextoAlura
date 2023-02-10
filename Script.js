aba4e884ece68318a508d243579dfea0343e43bb
Miguel <mikelpro2002@gmail.com>
2023-02-10 13:08:18 -0600

    subiendo proyecto alura encriptador terminado

--- Script.js
+++ Script.js
@@ -13,28 +13,33 @@ function encriptar(stringEncriptado){
 
     for(let i = 0 ; i < matrizCodigo.length; i ++){
         if(stringEncriptado.includes(matrizCodigo[i][0])){
-            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         };
-    }
+    };
     return stringEncriptado;
-}
+};
 
 function btnDesencriptar(){
     const textoDesencriptado = desencriptar(textArea.value);
     mensaje.value = textoDesencriptado;
     textArea.value = "";
-}
+};
 
 function desencriptar(stringDesencriptado){
-    let matrizCodigo = [["enter,", ""], ["ines", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]]
+    let matrizCodigo = [["enter,", ""], ["ines", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
     stringDesencriptado = stringDesencriptado;
 
     for(let i = 0 ; i < matrizCodigo.length; i ++){
         if(stringDesencriptado.includes(matrizCodigo[i][0])){
             stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
-    }
+    };
     return stringDesencriptado;
-}
+};
 
-//function copiar()
+//esta funcion copia el texto encriptado o desencriptado
+const textoAcopiar = mensaje
+function copiarTexto() {
+    textoAcopiar.select(); 
+    document.execCommand("copy");
+ };
